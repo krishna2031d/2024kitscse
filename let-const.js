9b495e76374095198e5fff472c57f39f5f77554d
GOLLA KRISHNA TEJA <140505460+krishna2031d@users.noreply.github.com>
2024-06-21 09:35:33 +0530

    msg

---
+++ let-const.js
@@ -0,0 +1,16 @@
+// let and const
+
+/*let first = 10
+first = 20
+console.log(first)
+
+const second = 30
+second = 40
+console.log(second)*/
+
+let krishna = {rollnumber: '550', name: 'krishna'}
+krishna.rollnumber = '650'
+console.log(krishna)
+const teja = {rollnumber: 'SH1', name: 'teja'}
+teja.rollnumber = 'SH2'
+console.log(teja)
